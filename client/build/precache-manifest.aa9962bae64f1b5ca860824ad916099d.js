3543c7aace2728c0b8e3718dc895be39ba9de37c
Hector Hernandez <hectorohernz@gmail.com>
2020-07-01 13:49:18 -0400

    adding

---
+++ client/build/precache-manifest.aa9962bae64f1b5ca860824ad916099d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927ed9068e736ec70e742e419e58fa06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95dabe23af05dedd500c",
+    "url": "/static/css/main.8d48d578.chunk.css"
+  },
+  {
+    "revision": "996d802eb4afc4774651",
+    "url": "/static/js/2.5e986ce9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5e986ce9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95dabe23af05dedd500c",
+    "url": "/static/js/main.89aa74d2.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
