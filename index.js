bda45eaaea15f7c38d09fb406b521988b9eda038
Hector Hernandez <hectorohernz@gmail.com>
2020-07-01 13:22:43 -0400

    adding stuf

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 5000; // Server location is localhost:5000 or If process.env.PORT
 const connectDB = require("./config/db"); // Requiring Connection to mongodb and mongoose connection
 const path = require("path");
-const proxy = require("http-proxy-middleware");
+const rootAPI = require('./routes/api/rootApi');
 
 // Connect Database
 connectDB();
 
-module.exports = function(app) {
-  app.use(proxy('/api/**', { target: 'http://localhost:5000' }));
-  app.use(proxy('/otherApi/**', { target: 'http://localhost:5000' }));
-};
+
 
 // using json data
 app.use(express.json({ extented: false, limit: "50mb" }));
@@ -21,7 +18,7 @@ app.use("/api/users", require("./routes/api/users")); // Defining User route
 app.use("/api/auth", require("./routes/api/auth")); // Defining Auth route
 app.use("/api/profile", require("./routes/api/profiles")); // Defining profiles route
 app.use("/api/posts", require("./routes/api/post")); // Defining Post route
-
+app.use('/', rootAPI)
 // Server static assets in production
 
 if (process.env.NODE_ENV === "production") {
