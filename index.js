033533bdfb8bc3d50384142e4ea4d160c1d08526
Hector Hernandez <hectorohernz@gmail.com>
2020-07-02 08:35:30 -0400

    rm proxy

--- index.js
+++ index.js
@@ -4,16 +4,11 @@ const PORT = process.env.PORT || 5000; // Server location is localhost:5000 or I
 const connectDB = require("./config/db"); // Requiring Connection to mongodb and mongoose connection
 const path = require("path");
 const cors = require("cors");
-const router = require('express').Router();
 // Connect Database
 connectDB();
 // using json data
 app.use(express.json({ extented: false, limit: "50mb" }));
 app.use(cors());
-
-router.use(function(req, res) {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 // Hello
 // Defining Routes
@@ -27,7 +22,7 @@ app.use("/api/posts", require("./routes/api/post")); // Defining Post route
 if (process.env.NODE_ENV === "production") {
   console.log(" Server Is In production")
   app.use(express.static("client/build"));
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     const index = path.resolve(__dirname, "client", "build", "index.html");
     res.sendFile(index);
   });
