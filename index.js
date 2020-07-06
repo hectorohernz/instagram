aa599b585b906167661aa953b6af527d6d0714d7
Hector Hernandez <hectorohernz@gmail.com>
2020-07-06 12:00:58 -0400

    adding if-env

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.use("/api/posts", require("./routes/api/post")); // Defining Post route
 // Server static assets in production
 
 if (process.env.NODE_ENV === "production") {
-  
   console.log(" Server Is In production")
   app.use(express.static(path.join(__dirname, 'client/build')));
 
