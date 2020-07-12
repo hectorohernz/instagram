a785deb5bca5d021c4abfc6cde36f81dba90d929
Hector Hernandez <hectorohernz@gmail.com>
2020-07-12 12:55:15 -0400

    adding server changes for deploy

--- index.js
+++ index.js
@@ -19,14 +19,13 @@ app.use("/api/profile", require('./routes/api/profiles')); // Defining profiles 
 app.use("/api/posts", require("./routes/api/post")); // Defining Post route
 
 // Server static assets in production
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get("*", (req, res) => {
-       res.sendFile(path.resolve(__dirname,  "client/build", "index.html"));
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
+  app.get("/*", (req, res) => {
+       res.sendFile(path.join(__dirname, "client", "build" , "index.html"));
   });
 
-const PORT = process.env.PORT || 5000; // Server location is localhost:5000 or If process.env.PORT
-
 // Int Server
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`PORT Is Listening on localhost:${PORT}`);
 });
