885550df050f55240451ba4a2af389243427f26e
Hector Hernandez <hectorohernz@gmail.com>
2020-07-12 13:04:36 -0400

    adding changs

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use("/api/posts", require("./routes/api/post")); // Defining Post route
 // Server static assets in production
   app.use(express.static(path.join(__dirname, "client", "build")));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
        res.sendFile(path.join(__dirname, "client", "build" , "index.html"));
   });
 
