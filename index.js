9cb400152ddad911ba4bbeefa1f025e2950476a0
Hector Hernandez <hectorohernz@gmail.com>
2020-07-12 14:43:56 -0400

    adding server

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/', require("./routes/api/rootApi"));
 
 // Server static assets in production
 
-  app.use(express.static('client/build')));
+  app.use(express.static('client/build'));
   app.get("*", (req, res) => {	 
     res.sendFile(path.resolve(__dirname,  "./client/build", "index.html"));
   });
